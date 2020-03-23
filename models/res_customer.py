8e631a3e9065665f2479d22f24701bd3e68c6c14
vanhuy174 <becodon174@gmail.com>
2020-03-23 16:59:26 +0700

    create form send email

--- models/res_customer.py
+++ models/res_customer.py
@@ -139,35 +139,4 @@ class Customers(models.Model):
             'context': ctx,
         }
 
-    @api.multi
-    def action_send_to_customer(self):
-        self.ensure_one()
-        ir_model_data = self.env['ir.model.data']
-        try:
-            template_id = ir_model_data.get_object_reference('DGIS', 'email_template_customer')[1]
-        except ValueError:
-            template_id = False
-        try:
-            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
-        except ValueError:
-            compose_form_id = False
-        print(template_id, compose_form_id)
-        ctx = {
-            'default_model': 'dgis.customer',
-            'default_res_id': self.ids[0],
-            'default_use_template': bool(template_id),
-            'default_template_id': template_id,
-            'default_composition_mode': 'comment',
-            'mark_so_as_sent': True,
-            'force_email': True
-        }
-        return {
-            'type': 'ir.actions.act_window',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'mail.compose.message',
-            'views': [(compose_form_id, 'form')],
-            'view_id': compose_form_id,
-            'target': 'new',
-            'context': ctx,
-        }
+
